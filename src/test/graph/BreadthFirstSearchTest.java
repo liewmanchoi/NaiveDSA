df3f19af9a1556be93277ed58812bc95c60e82fe
Liew Manchoi <liewmanchoi@gmail.com>
2018-10-22 20:09:20 +0800

    广度优先搜索BFS测试文件
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/test/graph/BreadthFirstSearchTest.java
@@ -0,0 +1,32 @@
+package graph;
+
+import ds.graph.WeightedUndirectedGraph;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BreadthFirstSearchTest {
+
+    @Test
+    public void search() {
+        WeightedUndirectedGraph<Integer> graph = new WeightedUndirectedGraph<>();
+
+        for (int i = 0; i <= 7; ++i) {
+            graph.addVertex(i);
+        }
+
+        graph.addEdge(0, 5, 1);
+        graph.addEdge(0, 2, 1);
+        graph.addEdge(0, 7, 1);
+        graph.addEdge(2, 6, 1);
+        graph.addEdge(6, 4, 1);
+        graph.addEdge(7, 4, 1);
+        graph.addEdge(7, 1, 1);
+        graph.addEdge(5, 4, 1);
+        graph.addEdge(5, 3, 1);
+        graph.addEdge(3, 4, 1);
+
+        BreadthFirstSearch<Integer> graphs = new BreadthFirstSearch<>(graph);
+        graphs.search(System.out::print);
+    }
+}
